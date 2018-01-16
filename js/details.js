a4a142916639214b56f45164198f5dc36974f1a0
v1w3 <mgodukaviwe@gmail.com>
2018-01-16 15:41:28 +0200

    Okay I think the details pages are looking good, next...RWD. Eish konje scroll behaviour on mobile for home page, ne?! Ok no problem. I think I can even do that last.

--- js/details.js
+++ js/details.js
@@ -120,7 +120,7 @@
 		{
 			points: '700,84.4 1047.1,685.6 352.9,685.6 352.9,685.6 352.9,685.6 352.9,685.6',
 			scaleX: .8,
-			scaleY: .9,
+			scaleY: .6,
 			rotate: 0,
 			tx: 0,
 			ty: 0,
@@ -165,11 +165,11 @@
 		},
 		{
 			points: '890.9,54.3 1081.8,385 890.9,715.7 509.1,715.7 318.2,385 509.1,54.3',
-			scaleX: 1,
-			scaleY: 1,
+			scaleX: 0.7,
+			scaleY: 0.7,
 			rotate: -45,
 			tx: 0,
-			ty: -50,
+			ty: 0,
 			fill: {
 				color: 'none',
 				duration: 500,
@@ -187,35 +187,12 @@
 			}
 		},
 		{
-			points: '983.4,101.6 779,385 983.4,668.4 416.6,668.4 611,388 416.6,101.9',
-			scaleX: 1,
-			scaleY: 1,
-			rotate: 145,
-			tx: 100,
-			ty: -50,
-			fill: {
-				color: 'none',
-				duration: 500,
-				easing: 'linear'
-			},
-			animation: {
-				points: {
-					duration: 500,
-					easing: 'easeOutExpo'
-				},
-				svg: {
-					duration: 1500,
-					easing: 'easeOutElastic'
-				}
-			}
-		},
-		{
-			points: '983.4,101.6 1255,385 983.4,668.4 416.6,668.4 157,368 416.6,101.9',
-			scaleX: .7,
+			points: '700,84.4 1047.1,685.6 352.9,685.6 352.9,685.6 352.9,685.6 352.9,685.6',
+			scaleX: .8,
 			scaleY: .7,
-			rotate: -70,
-			tx: -50,
-			ty: 50,
+			rotate: 90,
+			tx: 50,
+			ty: 0,
 			fill: {
 				color: 'none',
 				duration: 500,
@@ -233,10 +210,10 @@
 			}
 		},
 		// {
-		// 	points: '983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.9 416.6,101.9 416.6,101.9',
-		// 	scaleX: 1.2,
-		// 	scaleY: 1.2,
-		// 	rotate: 20,
+		// 	points: '700,84.4 1047.1,685.6 352.9,685.6 352.9,685.6 352.9,685.6 352.9,685.6',
+		// 	scaleX: .8,
+		// 	scaleY: .9,
+		// 	rotate: 180,
 		// 	tx: 0,
 		// 	ty: 0,
 		// 	fill: {
@@ -254,7 +231,30 @@
 		// 			easing: 'easeOutElastic'
 		// 		}
 		// 	}
-		// }
+		// },
+		{
+			points: '983.4,101.6 983.4,668.4 416.6,668.4 416.6,101.9 416.6,101.9 416.6,101.9',
+			scaleX: 0.8,
+			scaleY: 0.8,
+			rotate: 20,
+			tx: 0,
+			ty: 0,
+			fill: {
+				color: 'none',
+				duration: 500,
+				easing: 'linear'
+			},
+			animation: {
+				points: {
+					duration: 500,
+					easing: 'easeOutExpo'
+				},
+				svg: {
+					duration: 1500,
+					easing: 'easeOutElastic'
+				}
+			}
+		}
 	];
 	let step;
 
